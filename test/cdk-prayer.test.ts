6b4d49d1531ad278c7c29094fbdd8820dde0ff1f
Skux4life <mattellis1234@gmail.com>
2023-02-26 08:06:31 +1100

    Initial commit

---
+++ test/cdk-prayer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPrayer from '../lib/cdk-prayer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-prayer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPrayer.CdkPrayerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
